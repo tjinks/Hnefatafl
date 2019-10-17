e7d9e9a37d2979ce00a91fbaaf45c6bf61d6b50d
Tony Jinks <tony.jinks@gmail.com>
2019-10-17 20:29:27 +0100

    Initial Commit

---
+++ Hnefatafl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hnefatafl
+//
+//  Created by Tony Jinks on 17/10/2019.
+//  Copyright © 2019 Tony Jinks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hnefatafl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hnefatafl
+//
+//  Created by Tony Jinks on 17/10/2019.
+//  Copyright © 2019 Tony Jinks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
